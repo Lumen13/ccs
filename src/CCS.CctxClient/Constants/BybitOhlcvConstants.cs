b21202df1955b64fefff80976714adeadde775e5
Andrei Orlov <ntap13@gmail.com>
2025-10-31 11:41:46 +0300

    new constant-objects to process different timeIntervals

--- src/CCS.CctxClient/Constants/BybitOhlcvConstants.cs
+++ src/CCS.CctxClient/Constants/BybitOhlcvConstants.cs
@@ -6,7 +6,17 @@
 public static class BybitOhlcvConstants
 {
     public static readonly IReadOnlyCollection<OhlcvIntervalConstant> Values = [
-        new OhlcvIntervalConstant("30m", "20d", 959)
+        new OhlcvIntervalConstant("30m", "20d", 959),
+        new OhlcvIntervalConstant("1m", "16h", 959),
+        new OhlcvIntervalConstant("3m", "2d", 959),
+        new OhlcvIntervalConstant("5m", "3d", 863),
+        new OhlcvIntervalConstant("15m", "10d", 959),
+        new OhlcvIntervalConstant("1h", "40d", 959),
+        new OhlcvIntervalConstant("2h", "80d", 959),
+        new OhlcvIntervalConstant("4h", "160d", 959),
+        new OhlcvIntervalConstant("6h", "240d", 959),
+        new OhlcvIntervalConstant("12h", "480d", 959),
+        new OhlcvIntervalConstant("1d", "995d", 994)
         ];
 }
 
